864fc204e17ff18d684c7e17cfaf7445e3ceb647
Glizela Taino <glizelataino@Glizelas-MacBook-Pro.local>
2017-07-19 16:31:25 -0500

    loading questions dynamically

--- src/app/shared/model/innovation-brief.service.ts
+++ src/app/shared/model/innovation-brief.service.ts
@@ -12,20 +12,30 @@ export class InnovationBriefService {
 
   responses$: FirebaseListObservable<any>;
 
-  constructor(private db:AngularFireDatabase){}
+  currentUserInfo: any;
+
+  constructor(private db:AngularFireDatabase, private authService: AuthService){}
 
   setIBR(){
     console.log("setting ibr dict", this.ib_responses.getDict());
     // TODO: make user a var
-    const path = this.db.object("/responses/" + AuthService.UNKNOWN_USER.getUserId());
-    path.update({ [this.ib_responses.$key] : this.ib_responses.getDict() });
+    this.authService.authInfo$.subscribe(authInfo => {
+      const path = this.db.object("/responses/" + authInfo.getUserId());
+      path.update({ [this.ib_responses.$key] : this.ib_responses.getDict() });  
+    });
+    
   }
 
   getResponses(): Observable<InnovationBriefResponses>{
-    console.log("getting responses from: ", AuthService.UNKNOWN_USER.getUserId());
-    return this.db.object('responses/' + AuthService.UNKNOWN_USER.getUserId() + '/form0')
-      .do(val => console.log(val))
-      .map(val => InnovationBriefResponses.fromJson(val));
+
+    return this.authService.authInfo$.subscribe(authInfo => {
+      this.db.object('responses/' + authInfo.getUserId() + '/form0')
+        .do(val => console.log(val))
+        .map(val => InnovationBriefResponses.fromJson(val));
+      });
+    // return this.db.object('responses/' + AuthService.UNKNOWN_USER.getUserId() + '/form0')
+    //   .do(val => console.log(val))
+    //   .map(val => InnovationBriefResponses.fromJson(val));
   }
 
 }
